ce00dd6e2fcfbd25c6f9d2776183c3444ae33491
salena-till22 <salena.till@ufl.edu>
2024-04-16 16:15:29 -0400

    created BiBFS.cpp and BiBFS.h files

---
+++ src/BiBFS.cpp
@@ -0,0 +1,5 @@
+//
+// Created by salen on 4/16/2024.
+//
+
+#include "BiBFS.h"
---
+++ src/BiBFS.h
@@ -0,0 +1,14 @@
+//
+// Created by salen on 4/16/2024.
+//
+
+#ifndef WIKILINK_BIBFS_H
+#define WIKILINK_BIBFS_H
+
+
+class BiBFS {
+
+};
+
+
+#endif //WIKILINK_BIBFS_H
