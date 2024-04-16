02897f8248b742f0e3091d697c4aea8cfac843e2
Savannah Ogletree <127220162+savannaho8328@users.noreply.github.com>
2024-04-16 08:58:48 -0400

    Add files via upload
    updated the file, text, need font

--- main.cpp
+++ main.cpp
@@ -5,22 +5,34 @@
 using namespace std;
 using namespace sf;
 
-
+void setText(int window_width, Text title){
+    FloatRect titleRect = title.getLocalBounds();
+    title.setOrigin(titleRect.left +titleRect.width / 2.0f,
+                    titleRect.top + titleRect.height / 2.0f);
+    title.setPosition(Vector2f((window_width / 2.0f), (window_width / 2.0 - 150)));
+}
 
 int main() {
     int window_width = 700;
     sf::RenderWindow window(VideoMode(window_width, window_width), "Vertically Challenged Link", Style::Close);
     window.setFramerateLimit(60);
-    Font new_font;
+    Font Font;
     Text title;
-    title.setFont(new_font);
+    Font.loadFromFile("font_space_font.ttf");
+    title.setFont(Font);
     title.setString("Vertically Challened Link");
     title.setCharacterSize(26);
     title.setFillColor(Color::Black);
-    FloatRect titleRect = title.getLocalBounds();
-    title.setOrigin(titleRect.left +titleRect.width / 2.0f,
-                    titleRect.top + titleRect.height / 2.0f);
-    title.setPosition(Vector2f((window_width / 2.0f), (window_width / 2.0 - 150)));
+    setText(window_width, title);
+
+    Text header;
+    header.setString("Enter First Word:");
+    header.setFont(Font); // need help with the font
+    header.setCharacterSize(24);
+    header.setFillColor(Color::Magenta);
+    header.setStyle(Text::Bold);
+    setText(window_width, header);
+
 
     while (window.isOpen()){
         Event event;
