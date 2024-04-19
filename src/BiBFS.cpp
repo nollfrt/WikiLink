53cb8a4d77fa4665c56288ded865256f264106ac
salena-till22 <salena.till@ufl.edu>
2024-04-19 16:30:41 -0400

    updated BFS and BiBFS functions

--- src/BiBFS.cpp
+++ src/BiBFS.cpp
@@ -3,11 +3,13 @@
 //
 
 #include "BiBFS.h"
+#include "database.h"
 #include <iostream>
 #include <stdio.h>
 #include <queue>
 using namespace std;
 
+/*
 string BiBFS::get_Redirect(std::string s_title) {
     //find the actual article title that is associated with the redirect
     bool redirect = false;
@@ -21,14 +23,18 @@ string BiBFS::get_Redirect(std::string s_title) {
     }
     return s_title;
 }
+*/
 
 void BiBFS::bi_bfs(string start, string end) {
-    //check to see if the start is a redirect link
-    string new_start = get_Redirect(start);
+    database helper;
+
+    //get the id for the start
+    unsigned int start_id = helper.getID(start);
+
     //then push it into the queue
-    q.push(new_start);
+    q.push(start_id);
     while(!q.empty()) {
-        string u = q.front();
+        unsigned int u = q.front();
         q.pop();
         vector<unsigned int> neighbors;
         string list = SELECT outgoing_links FROM links;
@@ -48,5 +54,34 @@ void BiBFS::bi_bfs(string start, string end) {
             // is unique for each path and it doesn't carry over to another path search
         }
     }
+}
+void BiBFS::bfs(string start, string end) {
+    database helper;
 
+    //get the id for the start
+    unsigned int start_id = helper.getID(start);
+
+    //then push it into the queue
+    q.push(start_id);
+    while(!q.empty()) {
+        unsigned int u = q.front();
+        q.pop();
+        vector<unsigned int> neighbors;
+        string list = SELECT outgoing_links FROM links;
+        stringstream ss(list);
+        string token;
+        while(getline(ss, token, '|')) {
+            neighbors.push_back(stoi(token));
+        }
+        for (unsigned int pageID : neighbors) {
+            string new_start_ = get_Redirect(//of the specific title at that page ID);
+            if (visited.count(pageID) == 0) {
+                visited.insert(pageID);
+                q.push(pageID);
+            }
+            //this is the general code for a regular BFS
+            //figure out how to make this bidirectional, fix some of the comments I made, make sure that the set
+            // is unique for each path and it doesn't carry over to another path search
+        }
+    }
 }
